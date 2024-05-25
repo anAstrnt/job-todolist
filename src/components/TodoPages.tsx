c4366c82cc7097a6a16124d2c3ba14ead2945fa7
anAstrnt <shinohara.2516300@gmail.com>
2024-05-25 22:03:38 +0900

    💄 TodoPageの外枠デザインの作成

--- src/components/TodoPages.tsx
+++ src/components/TodoPages.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import styles from "./TodoPages.module.css";
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
 
 export const TodoPages = () => {
-  return <div>TodoPages</div>;
+  return (
+    <div className={styles.todoPages}>
+      <div className={styles.titleName}>
+        <p>Title</p>
+        <form className={styles.addTodoForm}>
+          <input
+            type="text"
+            name="inputTitle"
+            placeholder="Add Title"
+            className={styles.inputTodo}
+          />
+          <button type="submit" className={styles.addTodoButton}>
+            <AddCircleOutlinedIcon />
+          </button>
+        </form>
+      </div>
+      <div className={styles.todoStatus}>
+        <div className={styles.inProgress}>
+          <p>In Progress</p>
+        </div>
+        <div className={styles.notStarted}>
+          <p>Not Started</p>
+        </div>
+        <div className={styles.completed}>
+          <p>Completed</p>
+        </div>
+      </div>
+    </div>
+  );
 };
