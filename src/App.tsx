b9d460df769f56dc66a011f8f88886d70b6d3bb3
anAstrnt <shinohara.2516300@gmail.com>
2024-05-28 20:11:23 +0900

    💩 不要なパッケージをインストールしているのであとから消す

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
 import TitleAddBar from "./components/TitleAddBar";
 import { todoContext, TodosProvider } from "./context/Todos";
